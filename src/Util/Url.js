53808a00ff87df8a83f0d8f667bf39bfe163c986
mahesh2291 <gmahesh252525@gmail.com>
2025-05-13 16:35:27 -0400

    updated url for deployment

--- src/Util/Url.js
+++ src/Util/Url.js
@@ -1,3 +1,3 @@
 // export const BASE_URL="/api" // production
 
-export const BASE_URL=location.hostname==="localhost"? "http://localhost:3000" : "/api"
+export const BASE_URL=location.hostname==="localhost"? "http://localhost:3000" : "http://3.137.162.214/api"
